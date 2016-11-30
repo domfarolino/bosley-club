1e935583e73afd980073a9e1763c3d6e124ac687
domfarolino <domfarolino@gmail.com>
2016-11-29 20:52:48 -0500

    Small changes for static-lazy-loading

--- app.js
+++ app.js
@@ -24,15 +24,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-})
-
-// Routes
-app.get('/', pages.index);
-app.get('/about/', pages.index);
-app.get('/contact/', pages.index);
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 const lex = LEX.create({
   server: 'staging',
