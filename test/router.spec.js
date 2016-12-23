3041e25f8eaed897e2870250a94b219352e48b52
Dom Farolino <domfarolino@gmail.com>
2016-12-23 11:10:50 -0500

    Light setup for testing

---
+++ test/router.spec.js
@@ -0,0 +1,15 @@
+const expect = require('chai').expect;
+const Router = require('../lib/client/modules/Router');
+
+describe('router-suite', () => {
+  const router = new Router();
+
+  it('should asynchronously load a route properly', () => {
+    expect(true).to.be.true;
+  });
+
+  it('should maintain proper router state', () => {
+    expect(true).to.be.true;
+  });
+
+});
